66a7fdd17592568e4f60385fa913724046bbe7dc
Mukhammad Rakhimov <maometusu@gmail.com>
2024-04-19 02:33:15 +0900

    fixes

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ sudo systemctl stop piarmenu.service
 go build -o piarmenu
 sudo mv piarmenu /usr/local/bin/piarmenu
 sudo cp .env /usr/local/bin/.env_piarmenu
+sudo chown www-data:www-data /usr/local/bin/.env_piarmenu
 sudo systemctl start piarmenu.service
--- internal/interface/repository/file_repository/upload_image.go
+++ internal/interface/repository/file_repository/upload_image.go
@@ -67,7 +67,7 @@ func fileToImage(file multipart.File) (image.Image, error) {
 }
 
 func writeImageToFile(filename, directory string, img image.Image) error {
-	err := os.MkdirAll(directory, os.ModePerm)
+	err := os.MkdirAll(directory, 0700)
 	if err != nil {
 		return err
 	}
